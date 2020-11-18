74658e3f16a1e0188dd0ef8429960aa273a7c9e4
dev-grace <qkrdmsh24@naver.com>
2020-11-18 19:00:41 +0900

    Add 뜨먜며소

--- src/UserInfo/EmailAuth.js
+++ src/UserInfo/EmailAuth.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import "./signUp.css"
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class EmailAuth extends Component {
 
@@ -29,38 +31,71 @@ class EmailAuth extends Component {
           });
       }
 
+
     async onClickBtn(){ 
     var ret;
-    await axios.post('http://35.232.159.201:3000/api/auth/login',
+    await axios.post('http://35.232.159.201:3000/api/auth/verify',
     {
         "userId": this.state.userId,
         "password": this.state.password,
         "email": this.state.email
-        })
+    })
         .then(function (response) {
         ret = response.data;
-        console.log(ret.message);
-        console.log(ret);
+        console.log("ret.message : "+ret.msg);
+        console.log("ret : "+ret);
+        alert("이메일이 전송되었습니다. 링크를 확인해주세요");
     })
     .catch(function (error) {
         ret = error.response;
-        console.log(ret);
         ret = ret.data;
         console.log(ret);
+        console.log("error ret : "+ret.msg);
         alert("Error");
         //에러 페이지 세분화
     });
 
-    this.setState({ message: ret.message});
+    this.setState({ message: ret.msg});
     }
 
     render() {
         return (
-            <form>
-                <div>
-                
+            <div>
+                <div className="signup">
+                    <div className="header">
+                        <Link to="/" style={{ textDecoration: 'none' }}>
+                            <p className="bold">Sillock</p></Link>
+                        <div className="head">
+                            <p>sillock 계정 생성</p>
+                            <p className="etitle">이메일 인증</p>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <div>
+                            <p className="title2">기관검색</p>
+                            <p className="txt">증명서 발급을 원하시는 학교 혹은 기관명을 먼저 검색해주세요.</p>
+                        </div>
+                        <div>
+                            <form >
+                                <label># 아이디 <input type="text" name="userId" 
+                                    onChange={this.handleChange} placeholder="이름" required></input></label><br/><br/>
+                                                {/* <Button variant="contained" color="primary">중복확인</Button> */}
+                                <label>#  비밀번호 <input type="password" name="password" placeholder="비밀번호"
+                                    onChange={this.handleChange} required></input></label><br/><br/>
+                                <label>#   이메일 <input type="text" name="email" onChange={this.handleChange} 
+                                    placeholder="Email"  required></input><select name="items1">
+                                                                                <option value="naver.com">@naver.com</option>
+                                                                                <option value="google.com">@google.com</option>
+                                                                                <option value="daum.com">@daum.com</option>
+                                                                            </select></label>
+                            </form><br/><br/>
+                            <Button variant="contained"  onClick={(e) => { this.onClickBtn() }}>확인</Button><br/><br/>     
+                        
+                        -- 서버에서 온 값 : "{this.state.message}" 
+                        </div>
+                    </div>
                 </div>
-            </form>
+            </div>
             );
         }
     }
